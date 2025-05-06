a6e9bec9c1957e573e46a3dc1903d8af697333c8
ChalukaLOL <martgamer331040@gmail.com>
2025-05-06 14:49:49 +0700

    initial commit

--- pages/api/recipes.js
+++ pages/api/recipes.js
@@ -2,7 +2,7 @@ import { query } from '../../lib/db';
  
 export default async function handler(req, res) {
   try {
-    const recipes = await query('SELECT id, title, image_url FROM recipes');
+    const recipes = await query('SELECT id, title, ingredients, instructions, category, image_url FROM recipes');
     res.status(200).json({ data: recipes });
   } catch (error) {
     console.error('Error fetching recipes:', error);
